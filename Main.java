ad654ab68ae6c35775bfd065c76169aa2aa87748
JohnChamorro <john.chamorro@globant.com>
2024-06-26 15:41:11 -0500

    FirstVersion

---
+++ Main.java
@@ -0,0 +1,13 @@
+/* 
+Author: John Sebastian Chamorro Narvaez 
+Cod: 1940756
+Note: Entrega tarea #1 curso Metodologías de Desarrollo de Software
+*/
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World :D");
+    }
+
+}
